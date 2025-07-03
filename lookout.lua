7ed536c2f99ebcd08e60829ed36e73f63c20ce95
kyleschaub <kylejschaub@gmail.com>
2025-07-03 18:59:04 -0500

    Rename 'view' to 'look'

--- lookout.lua
+++ lookout.lua
@@ -5,22 +5,22 @@ local lookout = {
     _DESCRIPTION = "Parallax logic with multiple moving layers",
 }
 
-function lookout:newView(newLayerData, args)
-    local view = {}
-    view.x = 0 -- view perspective position
-    view.y = 0
-    view.spoofX = 0 -- automatic movement of the view position
-    view.spoofY = 0
+function lookout:create(newLayerData, args)
+    local look = {}
+    look.x = 0 -- look perspective position
+    look.y = 0
+    look.spoofX = 0 -- automatic movement of the look position
+    look.spoofY = 0
 
-    -- any additional properties you would like to set on the view
-    if args then for k, v in pairs(args) do view[k] = v end end
+    -- any additional properties you would like to set for the look
+    if args then for k, v in pairs(args) do look[k] = v end end
 
-    function view:reset()
-        view.layers = {}
-        view.layerData = nil
+    function look:reset()
+        look.layers = {}
+        look.layerData = nil
     end
 
-    function view:addLayerData(data)
+    function look:addLayerData(data)
         if not data then error("addLayerData requires a table of layers, or single layer") end
         if type(data) ~= "table" then error("addLayerData parameter must be a table") end
         if next(data) == nil then error("addLayerData parameter cannot be an empty table") end
@@ -30,7 +30,7 @@ function lookout:newView(newLayerData, args)
         for i, ld in ipairs(data) do table.insert(self.layerData, ld) end
     end
 
-    function view:newLayer(img, args)
+    function look:newLayer(img, args)
         if not img then error("newLayer requires an image") end
 
         -- Stores all data about the layer
@@ -43,17 +43,17 @@ function lookout:newView(newLayerData, args)
         layer.alpha = 1
         layer.drawSides = true -- draw duplicates on left and right
         layer.drawVertical = true -- draw duplicates on top and bottom
-        layer.view = view -- reference to the view this layer belongs to
+        layer.look = look -- reference to the look this layer belongs to
 
-        -- Stores the position of the view in the previous frame
-        layer.oldViewX = 0
-        layer.oldViewY = 0
+        -- Stores the position from the previous frame
+        layer.oldLookX = 0
+        layer.oldLookY = 0
 
-        -- Relative position of the layer compared to the view position
+        -- Relative position of the layer compared to the actual position
         layer.relX = 0
         layer.relY = 0
 
-        -- Number of pixels the view position has to move before the layer moves
+        -- Number of pixels the look position has to move before the layer moves
         layer.depth = 3
 
         -- Manual offset of the layer
@@ -76,17 +76,17 @@ function lookout:newView(newLayerData, args)
         function layer:update(dt) -- calculate parallax positioning
             if layer.anim then layer.anim:update(dt) end
 
-            if layer.view.spoofX ~= 0 then layer.spoofX = layer.spoofX + layer.view.spoofX * dt end
-            if layer.view.spoofY ~= 0 then layer.spoofY = layer.spoofY + layer.view.spoofY * dt end
+            if layer.look.spoofX ~= 0 then layer.spoofX = layer.spoofX + layer.look.spoofX * dt end
+            if layer.look.spoofY ~= 0 then layer.spoofY = layer.spoofY + layer.look.spoofY * dt end
 
-            local vpx, vpy = layer.view:getPosition() -- get the view position
+            local vpx, vpy = layer.look:getPosition() -- get the look position
             layer.x, layer.y = vpx, vpy
 
-            vpx = vpx + layer.spoofX -- apply spoofing to the view position
+            vpx = vpx + layer.spoofX -- apply spoofing to the look position
             vpy = vpy + layer.spoofY
 
-            local diffX = vpx - layer.oldViewX
-            local diffY = vpy - layer.oldViewY
+            local diffX = vpx - layer.oldLookX
+            local diffY = vpy - layer.oldLookY
 
             if not layer.fixedX then layer.relX = layer.relX - (diffX / layer.depth) end
             if not layer.fixedY then layer.relY = layer.relY - (diffY / layer.depth) end
@@ -97,15 +97,15 @@ function lookout:newView(newLayerData, args)
             if layer.relY < -1 * layer.height then layer.relY = layer.relY + layer.height end
             if layer.relY > layer.height then layer.relY = layer.relY - layer.height end
 
-            layer.oldViewX = vpx
-            layer.oldViewY = vpy
+            layer.oldLookX = vpx
+            layer.oldLookY = vpy
         end
         
         function layer:reset()
             self.relX = 0
             self.relY = 0
-            self.oldViewX = self.x
-            self.oldViewY = self.y
+            self.oldLookX = self.x
+            self.oldLookY = self.y
         end
         
         function layer:draw()
@@ -126,71 +126,71 @@ function lookout:newView(newLayerData, args)
             end      
         end
 
-        table.insert(view.layers, layer)
+        table.insert(look.layers, layer)
     end
 
-    function view:setScale(scale)
+    function look:setScale(scale)
         if not scale then return end
-        for i, layer in ipairs(view.layers) do
+        for i, layer in ipairs(look.layers) do
             layer.scale = scale
             layer.width = layer.baseWidth * layer.scale
             layer.height = layer.baseHeight * layer.scale
         end
     end
-    -- NOTE: call view:update and view:draw to see changes (applies to all set methods)
+    -- NOTE: call look:update and look:draw to see changes (applies to all set methods)
 
-    function view:setPosition(x, y)
-        if x then view.x = x end
-        if y then view.y = y end
+    function look:setPosition(x, y)
+        if x then look.x = x end
+        if y then look.y = y end
     end
 
-    function view:setSpoofDir(x, y)
-        if x then view.spoofX = x end
-        if y then view.spoofY = y end
+    function look:setSpoofDir(x, y)
+        if x then look.spoofX = x end
+        if y then look.spoofY = y end
     end
 
-    function view:getPosition()
-        return view.x, view.y
+    function look:getPosition()
+        return look.x, look.y
     end
 
-    function view:getNormalizedSpoofDir()
-        local len = math.sqrt(view.spoofX^2 + view.spoofY^2)
+    function look:getNormalizedSpoofDir()
+        local len = math.sqrt(look.spoofX^2 + look.spoofY^2)
         if len == 0 then return 0, 0 end
-        return view.spoofX / len, view.spoofY / len
+        return look.spoofX / len, look.spoofY / len
     end
 
-    function view:update(dt, x, y)
-        view:setPosition(x, y)
+    function look:update(dt, x, y)
+        look:setPosition(x, y)
 
-        for i, layer in ipairs(view.layers) do
-            layer:update(dt, view.x, view.y)
+        for i, layer in ipairs(look.layers) do
+            layer:update(dt, look.x, look.y)
         end
     end
 
-    function view:draw()
-        for i, layer in ipairs(view.layers) do
+    function look:draw()
+        for i, layer in ipairs(look.layers) do
             layer:draw()
         end
     end
 
-    function view:init(nld)
-        view:reset()
-        view:addLayerData(nld)
+    function look:init(nld)
+        look:reset()
+        look:addLayerData(nld)
 
-        for i, ld in ipairs(view.layerData) do -- create all layers
+        for i, ld in ipairs(look.layerData) do -- create all layers
             local img = ld.img
             ld.img = nil -- remove img from layer data
-            view:newLayer(img, ld)
+            look:newLayer(img, ld)
         end
     end
 
     if newLayerData then
-        view:init(newLayerData) -- initialize layer data
+        look:init(newLayerData) -- initialize layer data
     else
-        print("call view:init(newLayerData) to start the lookout view")
+        print("call look:init(newLayerData) to start")
     end
 
-    return view
+    return look
 end
 
 return lookout
--- main.lua
+++ main.lua
@@ -1,40 +1,41 @@
--- This project can be run as a Love2D game, where main.lua is the entry point.
+-- This project can be run as a Love2D game, main.lua is the starting point
 function love.load()
     local images = {} -- load images for the parallax layers
     images.far = love.graphics.newImage("stars/far.png")
     images.mid = love.graphics.newImage("stars/mid.png")
     images.near = love.graphics.newImage("stars/near.png")
 
-    -- Create the lookout view
+    -- Create the lookout
     local lookout = require("lookout")
     local layerData = {
         { img = images.far, depth = 8 }, -- depth represents how 'far away' the layer is
         { img = images.mid, depth = 4 }, -- lower depth means the layer moves faster
-        { img = images.near, depth = 2 }, -- at 0, the layer matches the view position
+        { img = images.near, depth = 2 }, -- at 0, the layer matches the look (view) position
     }
 
-    local viewArgs = { spoofX = 1000 } -- Initial spoof direction for the view
-    view = lookout:newView(layerData, viewArgs) -- stored in a global variable for easy access
-    view.font = love.graphics.newFont(16) -- Set a font for the view
+    local args = { spoofX = 1000 } -- Initial spoof direction for the lookout
+    look = lookout:create(layerData, args) -- lookout instance is stored in a global variable called 'look'
+    look.font = love.graphics.newFont(16) -- Set a font for the lookout
 end
 
 function love.update(dt)
     local mag = 1000
     local dirX, dirY = getDirFromKeyboard()
-    -- using the keyboard input, we can set the spoof direction for the view
-    if dirX ~= 0 or dirY ~= 0 then view:setSpoofDir(dirX * mag, dirY * mag) end
+
+    -- using the keyboard input, we can set the spoof direction of the lookout
+    if dirX ~= 0 or dirY ~= 0 then look:setSpoofDir(dirX * mag, dirY * mag) end
 
     local centerX, centerY = love.graphics.getWidth()/2, love.graphics.getHeight()/2
-    view:update(dt, centerX, centerY) -- viewpoint is the center of the screen
+    look:update(dt, centerX, centerY) -- lookout position is the center of the screen
     -- if there's a camera, use camX and camY instead of centerX and centerY
 end
 
 function love.draw()
     love.graphics.setColor(1,1,1,1)
-    view:draw() -- draws all parallax layers
+    look:draw() -- draws all parallax layers
     
-    love.graphics.setFont(view.font)
-    love.graphics.print("Steer with the arrow keys!", 10, 10)
+    love.graphics.setFont(look.font)
+    love.graphics.print("Use the arrow keys!", 10, 10)
 end
 
 function getDirFromKeyboard()
