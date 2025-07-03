836ef928596bf472d2cb2d831e6d1449cc45945d
kyleschaub <kylejschaub@gmail.com>
2025-07-03 16:21:08 -0500

    Added comments and better README

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # lookout
-Parallax logic with multiple moving layers
+Parallax logic with multiple moving layers, written in Lua.
+
+Lookout receives a list of layers (images) along with the corresponding depth value. Using this information, each layer is able to calculate its independent movement position based on its depth, along with the viewpoint position. A view can also be assigned a 'spoof' direction, which means the view will automatically offset over time (travel) based on the spoof X & Y properties.
+
+To use lookout, you can include this entire project, or copy/paste lookout.lua directly into your codebase. See main.lua for an example of how a parallax view can be created for a Love2D project. The inline comments walk through every step of the process.
--- main.lua
+++ main.lua
@@ -1,6 +1,6 @@
 -- This project can be run as a Love2D game, where main.lua is the entry point.
 function love.load()
-    local images = {}
+    local images = {} -- load images for the parallax layers
     images.far = love.graphics.newImage("stars/far.png")
     images.mid = love.graphics.newImage("stars/mid.png")
     images.near = love.graphics.newImage("stars/near.png")
@@ -8,28 +8,30 @@ function love.load()
     -- Create the lookout view
     local lookout = require("lookout")
     local layerData = {
-        { img = images.far, depth = 8 },
-        { img = images.mid, depth = 4 },
-        { img = images.near, depth = 2 },
+        { img = images.far, depth = 8 }, -- depth represents how 'far away' the layer is
+        { img = images.mid, depth = 4 }, -- lower depth means the layer moves faster
+        { img = images.near, depth = 2 }, -- at 0, the layer matches the view position
     }
 
-    local viewArgs = { spoofX = 1000 } -- Initial spoof direction
-    view = lookout:newView(layerData, viewArgs)
+    local viewArgs = { spoofX = 1000 } -- Initial spoof direction for the view
+    view = lookout:newView(layerData, viewArgs) -- stored in a global variable for easy access
     view.font = love.graphics.newFont(16) -- Set a font for the view
 end
 
 function love.update(dt)
     local mag = 1000
     local dirX, dirY = getDirFromKeyboard()
+    -- using the keyboard input, we can set the spoof direction for the view
     if dirX ~= 0 or dirY ~= 0 then view:setSpoofDir(dirX * mag, dirY * mag) end
 
     local centerX, centerY = love.graphics.getWidth()/2, love.graphics.getHeight()/2
-    view:update(dt, centerX, centerY)
+    view:update(dt, centerX, centerY) -- viewpoint is the center of the screen
+    -- if there's a camera, use camX and camY instead of centerX and centerY
 end
 
 function love.draw()
     love.graphics.setColor(1,1,1,1)
-    view:draw()
+    view:draw() -- draws all parallax layers
     
     love.graphics.setFont(view.font)
     love.graphics.print("Steer with the arrow keys!", 10, 10)
